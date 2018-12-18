ee669210a7194f78b36e2e4abd5bec03740424e0
JACK HAUGH <G00359747@gmit.ie>
2018-12-18 22:41:13 +0000

    finished the GarethEdwards class and added pics. Have my Top 5 Players class finished

--- RugbyApp_Main/RugbyApp_Main/GarethEdwards.xaml.cs
+++ RugbyApp_Main/RugbyApp_Main/GarethEdwards.xaml.cs
@@ -15,6 +15,23 @@ namespace RugbyApp_Main
 		public GarethEdwards ()
 		{
 			InitializeComponent ();
-		}
-	}
+            image1();
+            image2();
+        }
+
+        private void image1()
+        {
+            var assembly = typeof(MainPage);
+            string FileName = "RugbyApp_Main.Assets.garethedwards1.jpg";
+            garethedwards1.Source = ImageSource.FromResource(FileName, assembly);
+
+        }
+        private void image2()
+        {
+            var assembly = typeof(MainPage);
+            string FileName = "RugbyApp_Main.Assets.garethedwards2.jpg";
+            garethedwards2.Source = ImageSource.FromResource(FileName, assembly);
+
+        }
+    }
 }
