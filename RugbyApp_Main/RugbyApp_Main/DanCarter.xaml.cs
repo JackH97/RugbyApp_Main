6a8b95517c711768bcfa133a12f660926f812a7f
JACK HAUGH <G00359747@gmit.ie>
2018-12-18 01:13:18 +0000

    Created the DanCarter class for the Top5Players class

---
+++ RugbyApp_Main/RugbyApp_Main/DanCarter.xaml.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace RugbyApp_Main
+{
+	[XamlCompilation(XamlCompilationOptions.Compile)]
+	public partial class DanCarter : ContentPage
+	{
+		public DanCarter ()
+		{
+			InitializeComponent ();
+		}
+	}
+}
