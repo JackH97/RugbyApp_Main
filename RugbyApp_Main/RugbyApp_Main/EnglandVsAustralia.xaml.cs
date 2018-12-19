fb013468bad23c2cd7d20dcaad2db6cdf8608ef4
JACK HAUGH <G00359747@gmit.ie>
2018-12-19 17:38:26 +0000

    Finished EnglandVsAustralia Class for RugbyDerbies Class

---
+++ RugbyApp_Main/RugbyApp_Main/EnglandVsAustralia.xaml.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace RugbyApp_Main
+{
+	[XamlCompilation(XamlCompilationOptions.Compile)]
+	public partial class EnglandVsAustralia : ContentPage
+	{
+		public EnglandVsAustralia ()
+		{
+			InitializeComponent ();
+            image1();
+            image2();
+        }
+
+        private void image1()
+        {
+            var assembly = typeof(MainPage);
+            string FileName = "RugbyApp_Main.Assets.englandvsaustralia1.jpg";
+            englandvsaustralia1.Source = ImageSource.FromResource(FileName, assembly);
+
+        }
+        private void image2()
+        {
+            var assembly = typeof(MainPage);
+            string FileName = "RugbyApp_Main.Assets.englandvsaustralia2.jpg";
+            englandvsaustralia2.Source = ImageSource.FromResource(FileName, assembly);
+
+        }
+    }
+}
