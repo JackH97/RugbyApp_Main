f01b7a633e2371c440b74ad5ab444559b1ac8642
JACK HAUGH <G00359747@gmit.ie>
2018-12-19 16:51:45 +0000

    finished my NewZealandVsFrance class for RugbyDerbies class

---
+++ RugbyApp_Main/RugbyApp_Main/NewZealandVsFrance.xaml.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace RugbyApp_Main
+{
+	[XamlCompilation(XamlCompilationOptions.Compile)]
+	public partial class NewZealandVsFrance : ContentPage
+	{
+		public NewZealandVsFrance ()
+		{
+			InitializeComponent ();
+            image1();
+            image2();
+        }
+
+        private void image1()
+        {
+            var assembly = typeof(MainPage);
+            string FileName = "RugbyApp_Main.Assets.newzealandvsfrance1.jpg";
+            newzealandvsfrance1.Source = ImageSource.FromResource(FileName, assembly);
+
+        }
+        private void image2()
+        {
+            var assembly = typeof(MainPage);
+            string FileName = "RugbyApp_Main.Assets.newzealandvsfrance2.jpg";
+            newzealandvsfrance2.Source = ImageSource.FromResource(FileName, assembly);
+
+        }
+    }
+}
