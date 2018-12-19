32a7967ba2b6ed148e13ff8e2d403c4e6fa9e8eb
JACK HAUGH <G00359747@gmit.ie>
2018-12-19 00:40:31 +0000

    Finished my ireland class for RugbyTeams and added pictures

---
+++ RugbyApp_Main/RugbyApp_Main/Ireland.xaml.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace RugbyApp_Main
+{
+	[XamlCompilation(XamlCompilationOptions.Compile)]
+	public partial class Ireland : ContentPage
+	{
+		public Ireland ()
+		{
+			InitializeComponent ();
+            image1();
+            image2();
+        }
+
+        private void image1()
+        {
+            var assembly = typeof(MainPage);
+            string FileName = "RugbyApp_Main.Assets.ireland1.png";
+            ireland1.Source = ImageSource.FromResource(FileName, assembly);
+
+        }
+        private void image2()
+        {
+            var assembly = typeof(MainPage);
+            string FileName = "RugbyApp_Main.Assets.ireland2.jpg";
+            ireland2.Source = ImageSource.FromResource(FileName, assembly);
+
+        }
+    }
+}
