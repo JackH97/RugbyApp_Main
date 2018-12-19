6c66e6d44ac18e2a7e4f7dab1ee5d5dbb6b88be9
JACK HAUGH <G00359747@gmit.ie>
2018-12-19 19:35:29 +0000

    Finished DifferencesBetweenUandL Class and finished the UnionVsLeague Class. Also changed the heading in my EnglandVsWales class

---
+++ RugbyApp_Main/RugbyApp_Main/DifferenceBetweenUandL.xaml.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace RugbyApp_Main
+{
+	[XamlCompilation(XamlCompilationOptions.Compile)]
+	public partial class DifferenceBetweenUandL : ContentPage
+	{
+		public DifferenceBetweenUandL ()
+		{
+			InitializeComponent ();
+            image1();
+            image2();
+        }
+
+        private void image1()
+        {
+            var assembly = typeof(MainPage);
+            string FileName = "RugbyApp_Main.Assets.diff1.jpg";
+            diff1.Source = ImageSource.FromResource(FileName, assembly);
+
+        }
+        private void image2()
+        {
+            var assembly = typeof(MainPage);
+            string FileName = "RugbyApp_Main.Assets.diff2.jpg";
+            diff2.Source = ImageSource.FromResource(FileName, assembly);
+
+        }
+    }
+}
