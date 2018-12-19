c50fd16d045fa1a51efa0da3adc202d671008690
JACK HAUGH <G00359747@gmit.ie>
2018-12-19 03:09:10 +0000

    Created the RugbyWorldCup Class for the Tournament Class and have the Tournament class finished now

---
+++ RugbyApp_Main/RugbyApp_Main/RugbyWorldCup.xaml.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace RugbyApp_Main
+{
+	[XamlCompilation(XamlCompilationOptions.Compile)]
+	public partial class RugbyWorldCup : ContentPage
+	{
+		public RugbyWorldCup ()
+		{
+			InitializeComponent ();
+            image1();
+            image2();
+        }
+
+        private void image1()
+        {
+            var assembly = typeof(MainPage);
+            string FileName = "RugbyApp_Main.Assets.rugbyworldcup1.jpg";
+            rugbyworldcup1.Source = ImageSource.FromResource(FileName, assembly);
+
+        }
+        private void image2()
+        {
+            var assembly = typeof(MainPage);
+            string FileName = "RugbyApp_Main.Assets.rugbyworldcup2.jpg";
+            rugbyworldcup2.Source = ImageSource.FromResource(FileName, assembly);
+
+        }
+    }
+}
