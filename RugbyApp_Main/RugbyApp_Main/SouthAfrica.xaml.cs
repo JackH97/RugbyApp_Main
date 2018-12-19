8f5165601108ee9548f1311d89f1163b5a79b163
JACK HAUGH <G00359747@gmit.ie>
2018-12-19 00:59:20 +0000

    Finished my SouthAfrica Class and added Pictures

---
+++ RugbyApp_Main/RugbyApp_Main/SouthAfrica.xaml.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace RugbyApp_Main
+{
+	[XamlCompilation(XamlCompilationOptions.Compile)]
+	public partial class SouthAfrica : ContentPage
+	{
+		public SouthAfrica ()
+		{
+			InitializeComponent ();
+            image1();
+            image2();
+        }
+
+        private void image1()
+        {
+            var assembly = typeof(MainPage);
+            string FileName = "RugbyApp_Main.Assets.southafrica1.png";
+            southafrica1.Source = ImageSource.FromResource(FileName, assembly);
+
+        }
+        private void image2()
+        {
+            var assembly = typeof(MainPage);
+            string FileName = "RugbyApp_Main.Assets.southafrica2.jpg";
+            southafrica2.Source = ImageSource.FromResource(FileName, assembly);
+
+        }
+    }
+}
