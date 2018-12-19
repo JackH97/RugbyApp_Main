9bd84b85fe1b6f7f35060cb2f6fd8a32bf40caf4
JACK HAUGH <G00359747@gmit.ie>
2018-12-19 17:13:15 +0000

    Finished My LeinsterVsMunster Class for RugbyDerbies Class

---
+++ RugbyApp_Main/RugbyApp_Main/LeinsterVsMunster.xaml.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace RugbyApp_Main
+{
+	[XamlCompilation(XamlCompilationOptions.Compile)]
+	public partial class LeinsterVsMunster : ContentPage
+	{
+		public LeinsterVsMunster ()
+		{
+			InitializeComponent ();
+            image1();
+            image2();
+        }
+
+        private void image1()
+        {
+            var assembly = typeof(MainPage);
+            string FileName = "RugbyApp_Main.Assets.leinstervsmunster1.jpg";
+            leinstervsmunster1.Source = ImageSource.FromResource(FileName, assembly);
+
+        }
+        private void image2()
+        {
+            var assembly = typeof(MainPage);
+            string FileName = "RugbyApp_Main.Assets.leinstervsmunster2.jpg";
+            leinstervsmunster2.Source = ImageSource.FromResource(FileName, assembly);
+
+        }
+    }
+}
