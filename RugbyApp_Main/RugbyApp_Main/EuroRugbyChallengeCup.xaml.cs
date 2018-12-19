571ffb89e9ed79622ddd8ac3dac31a0927a9f162
JACK HAUGH <G00359747@gmit.ie>
2018-12-19 02:17:38 +0000

    Created the EuroRugbyChallengeCup class for the Tournaments class

---
+++ RugbyApp_Main/RugbyApp_Main/EuroRugbyChallengeCup.xaml.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace RugbyApp_Main
+{
+	[XamlCompilation(XamlCompilationOptions.Compile)]
+	public partial class EuroRugbyChallengeCup : ContentPage
+	{
+		public EuroRugbyChallengeCup ()
+		{
+			InitializeComponent ();
+            image1();
+            image2();
+        }
+
+        private void image1()
+        {
+            var assembly = typeof(MainPage);
+            string FileName = "RugbyApp_Main.Assets.eurochallengecup1.png";
+            eurochallengecup1.Source = ImageSource.FromResource(FileName, assembly);
+
+        }
+        private void image2()
+        {
+            var assembly = typeof(MainPage);
+            string FileName = "RugbyApp_Main.Assets.eurochallengecup2.jpg";
+            eurochallengecup2.Source = ImageSource.FromResource(FileName, assembly);
+
+        }
+    }
+}
