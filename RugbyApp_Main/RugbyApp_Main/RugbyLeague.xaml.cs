13c8f505e06612a681b5797754683bc3a9edad6e
JACK HAUGH <G00359747@gmit.ie>
2018-12-19 18:30:21 +0000

    finished the RugbyLeague Class for UnionVsLeague

---
+++ RugbyApp_Main/RugbyApp_Main/RugbyLeague.xaml.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace RugbyApp_Main
+{
+	[XamlCompilation(XamlCompilationOptions.Compile)]
+	public partial class RugbyLeague : ContentPage
+	{
+		public RugbyLeague ()
+		{
+			InitializeComponent ();
+            image1();
+            image2();
+        }
+
+        private void image1()
+        {
+            var assembly = typeof(MainPage);
+            string FileName = "RugbyApp_Main.Assets.rugbyleague1.jpg";
+            rugbyleague1.Source = ImageSource.FromResource(FileName, assembly);
+
+        }
+        private void image2()
+        {
+            var assembly = typeof(MainPage);
+            string FileName = "RugbyApp_Main.Assets.rugbyleague2.jpg";
+            rugbyleague2.Source = ImageSource.FromResource(FileName, assembly);
+
+        }
+    }
+}
