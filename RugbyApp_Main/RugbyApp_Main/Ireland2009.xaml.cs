eb08f1c79e8ecd717fce02ed295c91f1a177655d
JACK HAUGH <G00359747@gmit.ie>
2018-12-20 02:36:24 +0000

    Ireland2009 class finished for Top5RugbyMoments Class

---
+++ RugbyApp_Main/RugbyApp_Main/Ireland2009.xaml.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace RugbyApp_Main
+{
+	[XamlCompilation(XamlCompilationOptions.Compile)]
+	public partial class Ireland2009 : ContentPage
+	{
+		public Ireland2009 ()
+		{
+			InitializeComponent ();
+            image1();
+            image2();
+        }
+
+        private void image1()
+        {
+            var assembly = typeof(MainPage);
+            string FileName = "RugbyApp_Main.Assets.irelandgs1.jpg";
+            irelandgs1.Source = ImageSource.FromResource(FileName, assembly);
+
+        }
+        private void image2()
+        {
+            var assembly = typeof(MainPage);
+            string FileName = "RugbyApp_Main.Assets.irelandgs2.jpg";
+            irelandgs2.Source = ImageSource.FromResource(FileName, assembly);
+
+        }
+    }
+}
