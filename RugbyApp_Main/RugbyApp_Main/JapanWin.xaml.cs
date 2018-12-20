39bd9004d3ad8a2f321705c8b5e2c085fafb8f2d
JACK HAUGH <G00359747@gmit.ie>
2018-12-20 02:14:17 +0000

    JapanWin Class finished for Top5RugbyMoments Class

---
+++ RugbyApp_Main/RugbyApp_Main/JapanWin.xaml.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace RugbyApp_Main
+{
+	[XamlCompilation(XamlCompilationOptions.Compile)]
+	public partial class JapanWin : ContentPage
+	{
+		public JapanWin ()
+		{
+			InitializeComponent ();
+            image1();
+            image2();
+        }
+
+        private void image1()
+        {
+            var assembly = typeof(MainPage);
+            string FileName = "RugbyApp_Main.Assets.japan1.jpg";
+            japan1.Source = ImageSource.FromResource(FileName, assembly);
+
+        }
+        private void image2()
+        {
+            var assembly = typeof(MainPage);
+            string FileName = "RugbyApp_Main.Assets.japan2.jpg";
+            japan2.Source = ImageSource.FromResource(FileName, assembly);
+
+        }
+    }
+}
