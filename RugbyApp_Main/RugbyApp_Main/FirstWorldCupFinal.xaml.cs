5db7985a2b93180ba61fd60476c40ff3c9589095
JACK HAUGH <G00359747@gmit.ie>
2018-12-20 02:57:53 +0000

    FirstWorldCupFinal class is finished for Top5RugbyMoments Class

---
+++ RugbyApp_Main/RugbyApp_Main/FirstWorldCupFinal.xaml.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace RugbyApp_Main
+{
+	[XamlCompilation(XamlCompilationOptions.Compile)]
+	public partial class FirstWorldCupFinal : ContentPage
+	{
+		public FirstWorldCupFinal ()
+		{
+			InitializeComponent ();
+            image1();
+            image2();
+        }
+
+        private void image1()
+        {
+            var assembly = typeof(MainPage);
+            string FileName = "RugbyApp_Main.Assets.firstfinal1.jpg";
+            firstfinal1.Source = ImageSource.FromResource(FileName, assembly);
+
+        }
+        private void image2()
+        {
+            var assembly = typeof(MainPage);
+            string FileName = "RugbyApp_Main.Assets.firstfinal2.jpg";
+            firstfinal2.Source = ImageSource.FromResource(FileName, assembly);
+
+        }
+    }
+}
