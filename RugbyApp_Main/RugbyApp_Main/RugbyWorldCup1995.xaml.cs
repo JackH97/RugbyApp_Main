19d15b0b2381ceb39208437dd25f1955f1f43f67
JACK HAUGH <G00359747@gmit.ie>
2018-12-20 03:27:53 +0000

    Finished RugbyWorldCup1995 class for Top5RugbyMoments class and now that clas  is finished. Also found a mistake in LeinsterVsMunster class and has been fixed

---
+++ RugbyApp_Main/RugbyApp_Main/RugbyWorldCup1995.xaml.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace RugbyApp_Main
+{
+	[XamlCompilation(XamlCompilationOptions.Compile)]
+	public partial class RugbyWorldCup1995 : ContentPage
+	{
+		public RugbyWorldCup1995 ()
+		{
+			InitializeComponent ();
+            image1();
+            image2();
+        }
+
+        private void image1()
+        {
+            var assembly = typeof(MainPage);
+            string FileName = "RugbyApp_Main.Assets.Rugby951.jpg";
+            Rugby951.Source = ImageSource.FromResource(FileName, assembly);
+
+        }
+        private void image2()
+        {
+            var assembly = typeof(MainPage);
+            string FileName = "RugbyApp_Main.Assets.Rugby952.jpg";
+            Rugby952.Source = ImageSource.FromResource(FileName, assembly);
+
+        }
+    }
+}
